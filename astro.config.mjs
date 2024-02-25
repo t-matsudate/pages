dd94b932e36c9e5d20fcbc9aac219e0f2e87337b
t-matsudate <t.matsudate@gmail.com>
2024-02-25 19:05:12 +0900

    Added: ポートフォリオ第一段階(暫定版)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,26 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+import path from 'node:path';
+
+const stylesPath = path.resolve("src", "styles");
+const fontAwesomePath = path.resolve("node_modules", "@fortawesome", "fontawesome-free", "scss");
+const additionalData = `@import "${fontAwesomePath}/fontawesome";
+@import "${fontAwesomePath}/solid";
+@import "${fontAwesomePath}/brands";
+@import "${stylesPath}/common";
+`;
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), mdx(), sitemap()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: { additionalData }
+      }
+    }
+  }
+});
